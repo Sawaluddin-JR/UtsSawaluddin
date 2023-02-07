b0cf4985e22b00b05eb748edd1d55c5d6f30c970
asus <asus@LAPTOP-ILEMOPHU>
2023-02-08 00:33:11 +0700

    Add project files.

---
+++ UtsSawaluddin/Models/Genre.cs
@@ -0,0 +1,34 @@
+﻿namespace UtsSawaluddin.Models
+{
+    public class Genre
+    {
+        public JudulBuku JudulBuku { get; set; }
+
+        public Genre (JudulBuku judulBuku)
+        {
+            JudulBuku = judulBuku;
+        }
+        public string GenreBook
+        {
+            get
+            {
+                string genre = "";
+                if (JudulBuku == JudulBuku.KomikBlackClover || JudulBuku == JudulBuku.KomikOnePiece || JudulBuku == JudulBuku.KomikJujutsuKaisen)
+                {
+                    genre = "Komik";
+                }
+                else if (JudulBuku == JudulBuku.NovelAndreaHirata || JudulBuku == JudulBuku.NovelJKRowling || JudulBuku == JudulBuku.NovelTereLiye)
+                {
+                    genre = "Novel";
+                }
+                else if (JudulBuku == JudulBuku.SejarahIndonesia || JudulBuku == JudulBuku.SejarahYunani || JudulBuku == JudulBuku.SejarahJepang)
+                {
+                    genre = "Sejarah";
+                }
+                return genre;
+            }
+        }
+    }
+
+    
+}
---
+++ UtsSawaluddin/Models/JudulBuku.cs
@@ -0,0 +1,15 @@
+﻿namespace UtsSawaluddin.Models
+{
+    public enum JudulBuku
+    {
+        KomikOnePiece,
+        KomikBlackClover,
+        KomikJujutsuKaisen,
+        NovelAndreaHirata,
+        NovelJKRowling,
+        NovelTereLiye,
+        SejarahIndonesia,
+        SejarahYunani,
+        SejarahJepang
+    }
+}
